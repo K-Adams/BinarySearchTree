33ad7610c6e631fb52ec974416cb7ed438b802aa
Kollin <kbadams@uga.edu>
2015-10-07 16:56:46 -0400

    inital commit with blank readme.txt, bst.cpp, makefile

---
+++ README.txt
@@ -0,0 +1,6 @@
+Project 2 
+Binary Search Tree
+Kollin Adams
+Austin Wilburn
+
+blah blah blah
---
+++ bst.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std namespace;
+
+
+//here is our main for bst.cpp
+int main(int argv[], char* argc[])
+{
+
+}
