3192e93de56711ef7ddecf4f9a833b4aa3972496
awilburn <awilburn@uga.edu>
2015-10-09 18:00:14 -0400

    update3 by austin
    the list is having a hard time taking in nodes/objects. not sure about the syntax behind it

--- master.h
+++ master.h
@@ -6,10 +6,19 @@
 #include <cstdio>
 #include <string>
 #include <stdio.h>
-
+#include <list>
 using namespace std;
 ifstream file;
 int counter = 0;
+template<typename T>
+class Node {
+        T value;
+        Node<T> * right = nullptr;
+        Node<T> * left = nullptr;
+        Node<T> * parent = nullptr;
+        }; // Node
+template<class Node>
+std::list<Node*> tree_list;
 void openFile(string input)
 {
 
@@ -39,9 +48,48 @@ void closeFile(void)
       return;
     }
 }
-
-
-  //void insert(int node);
+int treeSize(void){
+        int s = tree_list.size();
+        return s;
+}
+template<typename T>
+  void insert(T data){
+        int i = 1;
+        Node<T> * n = new Node<T>;
+        n->value = data;
+        Node<T> * temp = new Node<T>;
+        if(tree_list.empty())
+                tree_list.push_front(n);
+        else {
+                temp = tree_list.front();
+                while(i){
+                        if((n->value)>(temp->value)&&temp->right!=NULL)
+                                temp=temp->right;
+                        else if((n->value)>(temp->value)&&temp->right==NULL){
+                                n = temp->right;
+                                n->parent = temp;
+                                i = 0;
+                                tree_list.push(n);
+                                }
+                        else if((n->value)<(temp->value)&&temp->left!=NULL)
+                                temp=temp->left;
+                        else if((n->value)<(temp->value)&&temp->left==NULL){
+                                n = temp->left;
+                                n->parent = temp;
+                                i = 0;
+                                tree_list.push(n);}
+                        else if(n->value ==temp->value)
+                                i = 0;
+                        }
+        }
+}
+void preorder(Node *n){
+        if(n){
+        cout<< n->value << " ";
+        preorder(n->left);
+        preorder(n->right);
+        }
+        }
   //void delete(int node);
   void readFile(void)
   {
@@ -63,3 +111,4 @@ void closeFile(void)
 }
 return;
 }
+                 
